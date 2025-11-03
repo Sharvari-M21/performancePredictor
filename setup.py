b6fb6a33730439240ca52f978d0d9c31c074d494
Sharvari-M21 <sharvarimashalkar21@gmail.com>
2025-11-03 22:51:11 +0530

    added logger and exception

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages,setup
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        HYPEN_E_DOT = '-e .'
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+setup(
+    name='ml_project',
+    version ='0.1.0',
+    author='Shar',
+    author_email= 'sharvarimashalkar21@gmail.com',
+    find_packages = find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    
+    ) 
